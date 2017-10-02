7300d5f18c08e865170e7eb45e1b4aed39913033
ahmedkhattab <ahmedkhattab@cb5109-39.labs.sce.carleton.ca>
2017-10-02 08:45:02 -0400

    Small Improvements

---
+++ src/BuddyInfo.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+
+public class BuddyInfo {
+	
+	private String name;
+	private String address;
+	private int number;
+	
+	
+	public BuddyInfo(String name, String address, int number) {
+		
+		super();
+		this.name = name;
+		this.address = address;
+		this.number = number;
+		
+		
+	}
+	
+	public static void main( String[] args) {
+		
+		
+	}
+		
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getAddress() {
+			return address;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public int getNumber() {
+			return number;
+		}
+
+		public void setNumber(int number) {
+			this.number = number;
+		}
+		
+	
+	}
+	
+		
+		
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
